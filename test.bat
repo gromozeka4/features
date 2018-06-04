22d3b4039601722d0a9d2bf9907cd85b9aa265f9
gromozeka4 <gromozeka4@gmail.com>
2018-06-04 14:59:46 +0300

    update

--- test.bat
+++ test.bat
@@ -1,1 +1,1 @@
-C:\Users\test\AppData\Local\Programs\Python\Python36-32\python.exe -m behave c:\behave\features\Read_Socket.feature
+C:\Users\test\AppData\Local\Programs\Python\Python36-32\python.exe -m behave c:\behave\features\Read_Socket.feature --junit
